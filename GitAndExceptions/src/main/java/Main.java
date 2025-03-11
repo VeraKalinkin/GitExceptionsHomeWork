28094a89e19657e825dd8d93c0cea5ebbd1a669b
Vera Kalinkin <vera-kalinkina11@rambler.ru>
2025-03-11 13:02:06 +0400

    exception added

--- GitAndExceptions/src/main/java/Main.java
+++ GitAndExceptions/src/main/java/Main.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        scanner.close();
+
         try {
-            someException();
-        } catch (SomeException se){
-            System.out.println(se.getMessage());
+            numberFourException(number);
+        } catch (UnluckyNumberException une){
+            System.out.println(une.getMessage());
         }
     }
 
-    public static void someException() throws SomeException{
-        throw new SomeException();
+    public static void numberFourException(int number) throws UnluckyNumberException{
+        if (number == 4)
+            throw new UnluckyNumberException();
     }
 }
---
+++ GitAndExceptions/src/main/java/UnluckyNumberException.java
@@ -0,0 +1,2 @@
+public class UnluckyNumberException  extends Exception{
+}
